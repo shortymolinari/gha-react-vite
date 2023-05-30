b0ce15b739461405a40560dfb6f81bc76729f151
Edwin Arley Rincón Henao <shortymolinari@gmail.com>
2023-05-30 15:07:29 -0500

    Fix: bugs

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('roots')
 )
