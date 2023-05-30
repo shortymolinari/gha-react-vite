ae25f883772c52ec22abfaeea3d9dc32a891549a
Edwin Arley Rincón Henao <shortymolinari@gmail.com>
2023-05-30 15:02:39 -0500

    Feat: Changes with error to test gh actionse

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // si uso un dominio propio la config queda asi => base: '/',
   base: 'https://shortymolinari.github.io/gha-react-vite',
   plugins: [react()],
 })
