8322954ab97c2bdc7ef3f48023411bdd3e1a7593
Edwin Arley Rincón Henao <shortymolinari@gmail.com>
2023-05-30 14:38:39 -0500

    Fix: project vite.js base config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://shortymolinari.github.io/',
+  base: 'https://shortymolinari.github.io/gha-react-vite',
   plugins: [react()],
 })
